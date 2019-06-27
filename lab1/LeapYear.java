2f1c1547cc942abbf472e3c9f07ecd8021d11b42
魏卓 <alva@weizhuos-MacBook-Pro.local>
2019-06-27 15:18:07 -0500

    completed first part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -7,9 +7,10 @@ public class LeapYear {
      *  @param  year to be analyzed
      */
     public static boolean isLeapYear(int year){
-	if ((year % 4 == 0){
+	if ((year % 400 == 0)||(year % 4 == 0 && year % 100 !=0 )){
 	    return (True)
 	}
+	else return (False)
     }
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
