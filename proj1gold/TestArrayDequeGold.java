7ef0471ebd0228bc675d4b398141257f98b608bb
alva <zwei22@wustl.edu>
2019-07-21 20:07:43 -0500

    proj1 c

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -2,94 +2,44 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 public class TestArrayDequeGold {
     @Test
-    public void testAddFirst() {
+    public void test() {
+        String msg = "\n";
         ArrayDequeSolution<Integer> ans = new ArrayDequeSolution();
         StudentArrayDeque<Integer> samp = new StudentArrayDeque();
-        for (int i = 0; i < 30; i += 1) {
-            int Int = StdRandom.uniform(100);
-            ans.addFirst(Int);
-            samp.addFirst(Int);
-        }
 
-        for (int i=0; i<30; i++) {
-            int actual = ans.get(i);
-            int expected = samp.get(i);
-            assertEquals("Oh noooo!\nThis is bad in addFirst():\n   Random number " + actual
-                            + " not equal to " + expected + "!",
-                    expected, actual);
+        for (int i = 0; i < 1000; i++) {
+            double test = StdRandom.uniform(4);
+
+            if (test == 0) {
+                int Int = StdRandom.uniform(100);
+                ans.addFirst(Int);
+                samp.addFirst(Int);
+                msg = msg + "addFirst(" + Int + ")\n";
+
+            }
+
+            else if (test == 1) {
+                int Int = StdRandom.uniform(100);
+                ans.addLast(Int);
+                samp.addLast(Int);
+                msg = msg + "addLast(" + Int + ")\n";
+            }
+
+            else if (test == 2) {
+                if (ans.size() != 0) {
+                    ans.removeFirst();
+                    samp.removeFirst();
+                    msg = msg + "removeFirst()\n";
+                    assertEquals(msg, ans.removeFirst(), samp.removeFirst());
+                }
+            }
+
+            else if (ans.size() != 0) {
+                ans.removeLast();
+                samp.removeLast();
+                msg = msg + "removeLast()\n";
+                assertEquals(msg, ans.removeLast(), samp.removeLast());
+            }
         }
     }
-
-    @Test
-    public void testAddLast() {
-        ArrayDequeSolution<Integer> ans = new ArrayDequeSolution();
-        StudentArrayDeque<Integer> samp = new StudentArrayDeque();
-        for (int i = 0; i < 30; i += 1) {
-            int Int = StdRandom.uniform(100);
-            ans.addLast(Int);
-            samp.addLast(Int);
-        }
-
-        for (int i = 0; i < 30; i++) {
-            int actual = ans.get(i);
-            int expected = samp.get(i);
-            assertEquals("Oh noooo!\nThis is bad in addLast():\n   Random number " + actual
-                            + " not equal to " + expected + "!",
-                    expected, actual);
-        }
-    }
-
-    @Test
-    public void testRemoveFirst() {
-        ArrayDequeSolution<Integer> ans = new ArrayDequeSolution();
-        StudentArrayDeque<Integer> samp = new StudentArrayDeque();
-        for (int i = 0; i < 30; i += 1) {
-            int Int = StdRandom.uniform(100);
-            ans.addFirst(Int);
-            samp.addFirst(Int);
-        }
-         ans.removeFirst();
-        samp.removeFirst();
-       // assertEquals("Oh noooo!\nThis is bad in removeFirst():\n   Removed item " + act
-                       // + " not equal to " + exp + "!", exp, act);
-        if (ans.size() != samp.size()) {
-            assertEquals("Oh noooo!\nThis is bad in removeFirst():\n   Size " + samp.size()
-                    + " not equal to " + ans.size() + "!", samp.size(), ans.size());
-        }
-        else for (int i = 0; i < 29; i++) {
-            int actual = ans.get(i);
-            int expected = samp.get(i);
-            assertEquals("Oh noooo!\nThis is bad in removeFirst():\n   Random number " + actual
-                            + " not equal to " + expected + "!",
-                    expected, actual);
-        }
-    }
-    @Test
-    public void testRemoveLast() {
-        ArrayDequeSolution<Integer> ans = new ArrayDequeSolution();
-        StudentArrayDeque<Integer> samp = new StudentArrayDeque();
-        for (int i = 0; i < 30; i += 1) {
-            int Int = StdRandom.uniform(100);
-            ans.addLast(Int);
-            samp.addLast(Int);
-        }
-        Integer exp = ans.removeLast();
-        Integer act = samp.removeLast();
-        //assertEquals("Oh noooo!\nThis is bad in removeLirst():\n   Removed item " + act
-                //+ " not equal to " + exp + "!", exp, act);
-        if (ans.size() != samp.size()) {
-            assertEquals("Oh noooo!\nThis is bad in removeFirst():\n   Size " + samp.size()
-                    + " not equal to " + ans.size() + "!", samp.size(), ans.size());
-        }
-
-        else for (int i = 0; i < 29; i++) {
-            int actual = ans.get(i);
-            int expected = samp.get(i);
-            assertEquals("Oh noooo!\nThis is bad in removeLast():\n   Random number " + actual
-                            + " not equal to " + expected + "!",
-                    expected, actual);
-        }
-
-    }
-
 }
