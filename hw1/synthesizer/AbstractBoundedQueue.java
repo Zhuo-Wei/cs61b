18b1f6c7df85f8bd7d6b51c3ebaeb4890a09088a
alva <zwei22@wustl.edu>
2019-07-25 15:47:21 -0500

    hw1 finished

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -3,10 +3,13 @@ package synthesizer;
 public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     protected int fillCount;
     protected int capacity;
+
+    @Override
     public int capacity() {
         return this.capacity;
     }
 
+    @Override
     public int fillCount() {
         return this.fillCount;
     }
--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,23 +1,32 @@
+// Make sure to make this class a part of the synthesizer package
 package synthesizer;
 import java.util.Iterator;
 
-public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>  {
-    /* Index for the next dequeue or peek. */
-    private int first;            // index for the next dequeue or peek
-    /* Index for the next enqueue. */
+//Make sure to make this class and all of its methods public
+// Make sure to make this class extend AbstractBoundedQueue<t>
+
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek.*/
+    private int first;
+    /* Index for the next enqueue */
     private int last;
-    /* Array for storing the buffer data. */
+    /* Array for storing the buffer data */
     private T[] rb;
 
     /**
      * Create a new ArrayRingBuffer with the given capacity.
      */
     public ArrayRingBuffer(int capacity) {
-        rb = (T[]) new Object[capacity];
+        //  Create new array with capacity elements.
+        //       first, last, and fillCount should all be set to 0.
+        //       this.capacity should be set appropriately. Note that the local variable
+        //       here shadows the field we inherit from AbstractBoundedQueue, so
+        //       you'll need to use this.capacity to set the capacity.
+        this.fillCount = 0;
         first = 0;
-        last = 0;
-        fillCount = 0;
+        last =0 ;
         this.capacity = capacity;
+        rb = (T[]) new Object[capacity];
     }
 
     /**
@@ -25,17 +34,23 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>  {
      * throw new RuntimeException("Ring buffer overflow"). Exceptions
      * covered Monday.
      */
-    @Override
+    private int plus1 (int x) {
+        if (x == (capacity -1)) {
+            return 0;
+        }
+
+        else return (x + 1);
+    }
+
     public void enqueue(T x) {
+        // TEnqueue the item. Don't forget to increase fillCount and update last.
         if (isFull()) {
-            throw new RuntimeException("Ring buffer overflow");
+            throw new RuntimeException("Ring Buffer Overflow");
         }
+
         rb[last] = x;
-        fillCount++;
-        last++;
-        if (last >= capacity) {
-            last = 0;
-        }
+        last = plus1(last);
+        fillCount += 1;
     }
 
     /**
@@ -43,57 +58,51 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>  {
      * throw new RuntimeException("Ring buffer underflow"). Exceptions
      * covered Monday.
      */
-    @Override
     public T dequeue() {
+        //  Dequeue the first item. Don't forget to decrease fillCount and update
         if (isEmpty()) {
             throw new RuntimeException("Ring buffer underflow");
         }
-        T res = rb[first];
+        T temp = rb[first];
         rb[first] = null;
-        fillCount--;
-        first++;
-        if (first >= capacity) {
-            first = 0;
-        }
-
-        return res;
+        first = plus1(first);
+        fillCount -= 1;
+        return temp;
     }
 
     /**
      * Return oldest item, but don't remove it.
      */
-    @Override
     public T peek() {
         if (isEmpty()) {
             throw new RuntimeException("Ring buffer underflow");
         }
+        // Return the first item. None of your instance variables should change.
         return rb[first];
     }
 
-    private class ArbIterator implements Iterator<T> {
-        private int ptr;
-        private int count;
-        ArbIterator() {
-            ptr = first;
-            count = 0;
+    // When you get to part 5, implement the needed code to support iteration.
+    private class iterator implements Iterator<T>{
+        private int i;
+        private int pos;
+        public iterator() {
+            i = 0;
         }
+
         public boolean hasNext() {
-            return count < fillCount();
-        }
-        public T next() {
-            T res = rb[ptr];
-            ptr++;
-            count++;
-            if (ptr >= capacity()) {
-                ptr = 0;
-            }
-            return res;
+            return i < fillCount();
         }
 
+        public T next(){
+            T returnItem = rb[pos];
+            pos = plus1(pos);
+            i =+ 1;
+            return returnItem;
+        }
     }
-
     @Override
     public Iterator<T> iterator() {
-        return new ArbIterator();
+        return new iterator();
     }
 }
+
